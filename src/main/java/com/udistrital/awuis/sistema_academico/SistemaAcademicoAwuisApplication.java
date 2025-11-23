eab706fdda976cdc076be324b5cc95f0ab57d394
anunezb18 <anunezb@udistrital.edu.co>
2025-11-22 22:27:38 -0500

    Commit inicial: Base inicial del proyecto de fis

---
+++ src/main/java/com/udistrital/awuis/sistema_academico/SistemaAcademicoAwuisApplication.java
@@ -0,0 +1,13 @@
+package com.udistrital.awuis.sistema_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAcademicoAwuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAcademicoAwuisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udistrital/awuis/sistema_academico/SistemaAcademicoAwuisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udistrital.awuis.sistema_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAcademicoAwuisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
